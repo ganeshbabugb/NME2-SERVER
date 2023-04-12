c970d34af3eaa1bba67f2aaf305a763597d202e1
Ganesh Babu <ganeshbabugb2525@gamil.com>
2023-04-12 20:27:37 +0530

    Modified Register API

--- server.js
+++ server.js
@@ -111,8 +111,7 @@ app.post("/api/login", (req, res) => {
   });
 });
 
-//working
-// Define API endpoint for registering a student for a course
+//REGISTER API
 app.post("/api/register-course", (req, res) => {
   const {
     register_id,
@@ -133,38 +132,50 @@ app.post("/api/register-course", (req, res) => {
       console.error(err);
       res.status(500).send("INTERNAL SERVER ERROR");
     } else if (selectResults.length > 0) {
-      // User does exist, insert the user into the users table and update the register count for the course
-      // const register_id = selectResults[0].id;
-      const userQuery =
-        "INSERT INTO student_details (register_id, name, email_id, course, course_selected, department, phone_no) VALUES (?, ?, ?, ?, ?, ?, ?)";
-      const userValues = [
-        register_id,
-        name,
-        email_id,
-        course,
-        course_selected,
-        department,
-        phone_no,
-      ];
-      db.query(userQuery, userValues, (err, userResults) => {
+      // User does exist, check if user is already registered in the `student_details` table using `register_id`
+      const userExistsQuery = `
+        SELECT * FROM student_details WHERE register_id = ?;
+      `;
+      db.query(userExistsQuery, [register_id], (err, userExistsResults) => {
         if (err) {
           console.error(err);
           res.status(500).send("INTERNAL SERVER ERROR");
+        } else if (userExistsResults.length > 0) {
+          res.status(400).send("USER ALREADY REGISTERED FOR A COURSE");
         } else {
-          const courseQuery = `
-            UPDATE ${course}_courses
-            SET register_count = register_count + 1
-            WHERE value = ?
-              AND register_count < 60;
-          `;
-          db.query(courseQuery, [course_selected], (err, courseResults) => {
+          // User does not exist in `student_details` table, insert the user into the table and update the register count for the course
+          const userQuery =
+            "INSERT INTO student_details (register_id, name, email_id, course, course_selected, department, phone_no) VALUES (?, ?, ?, ?, ?, ?, ?)";
+          const userValues = [
+            register_id,
+            name,
+            email_id,
+            course,
+            course_selected,
+            department,
+            phone_no,
+          ];
+          db.query(userQuery, userValues, (err, userResults) => {
             if (err) {
               console.error(err);
               res.status(500).send("INTERNAL SERVER ERROR");
-            } else if (courseResults.affectedRows === 0) {
-              res.status(403).send("COURSE IS FULL");
             } else {
-              res.send("COURSE REGISTERED SUCCESSFULLY");
+              const courseQuery = `
+                UPDATE ${course}_courses
+                SET register_count = register_count + 1
+                WHERE value = ?
+                  AND register_count < 60;
+              `;
+              db.query(courseQuery, [course_selected], (err, courseResults) => {
+                if (err) {
+                  console.error(err);
+                  res.status(500).send("INTERNAL SERVER ERROR");
+                } else if (courseResults.affectedRows === 0) {
+                  res.status(403).send("COURSE IS FULL");
+                } else {
+                  res.send("COURSE REGISTERED SUCCESSFULLY");
+                }
+              });
             }
           });
         }
